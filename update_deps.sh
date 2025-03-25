99499abd4af447895d57e1e484eebc1e3836a08f
Zhehao 0xFF <zhehao0827@163.com>
2025-03-25 10:38:59 +0800

    refactor: use trait op overloading instead of fn op overloading; use trait alias and fn alias simplify implementation

--- update_deps.sh
+++ update_deps.sh
@@ -1,9 +1,7 @@
+rm -rf .mooncakes.io
 moon update
 moon remove Luna-Flow/luna-generic
+moon remove moonbitlang/quickcheck
 moon add Luna-Flow/luna-generic
-
-# notice that if deps>2, you need write:
-
-# rm -rf .mooncakes.io
-# moon update
-# moon install
+moon add moonbitlang/quickcheck
+moon install
